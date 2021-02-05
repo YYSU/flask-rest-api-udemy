f3c5cc82037fb2b1eb2d3d58900faa3a24a2d3dc
YYSU <jim123820@gmail.com>
2021-02-05 21:20:40 +0800

    improve code quality

--- section4/app.py
+++ section4/app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class Studnet(Resource):
+items = []
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return { 'item': None }, 200 if item else 404
 
-api.add_resource(Studnet, '/student/<string:name>') # http//:127.0.0.1/student/<string:name>
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return { 'message': "An item with name {} already exists.".format(name) }, 400
+        
+        data = request.get_json(force=True)
+        item = { 'name': name, 'price': data['price'] }
+        items.append(item)
+        return item, 201
 
-app.run(port=5000)
+
+class ItemList(Resource):
+    def get(self):
+        return { 'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
